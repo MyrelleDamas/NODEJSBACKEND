b73d91428bda4cbeab6de4c3a96657861fdee5ad
MyrelleDamas <107486021+MyrelleDamas@users.noreply.github.com>
2023-08-10 09:13:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const {default: agent} = require('skywalking-backend-js');
+agent.start({
+        serviceName: 'GMMI::TESTE-NODE-BACKEND-MANUAL',
+        collectorAddress: '10.0.28.209:11800',
+});
+
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('<h1>Testando Node, diga olá!</h1>')
+})
+
+app.listen(8089, function () {
+  console.log('Listening on port 8089...')
+})
